5f1bab8a4f422485562957c2bd7f3e9af13cfd70
jly <453825818@qq.com>
2021-05-19 10:39:44 +0800

    todolist-vuex

--- 06-todolist-vuex/todolist-vuex/src/components/list/store/mutations.js
+++ 06-todolist-vuex/todolist-vuex/src/components/list/store/mutations.js
@@ -1,13 +1,17 @@
 import { LOADITEM } from './type'
+
 export default {
         [LOADITEM](state,payload){//state是state.js中的数据，payload是commit方法第二个参数
-           this.state.list = payload
+         //this是整个应用store。
+        // console.log(this.state.todolist.list == state.list) //true
+        // this.state.list = payload
+            state.list = payload
         },
         addItem(state,payload){
-            this.state.list.push(payload)
+            state.list.push(payload)
         },
         delItem(state,payload){
-           const list = this.state.list.filter(item=> item.id != payload)
-           this.state.list = list
+           const list = state.list.filter(item=> item.id != payload)
+           state.list = list
         }
 }
